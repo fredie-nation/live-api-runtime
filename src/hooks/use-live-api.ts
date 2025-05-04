de715550b8fddcfdbb9f4ef10f08a8d242041607
Fredie Nation <fredienation@gmail.com>
2025-05-04 20:26:25 +0700

    Update use-live-api.ts

--- src/hooks/use-live-api.ts
+++ src/hooks/use-live-api.ts
@@ -46,7 +46,8 @@ export function useLiveAPI({
 
   const [connected, setConnected] = useState(false);
   const [config, setConfig] = useState<LiveConfig>({
-    model: "models/gemini-2.0-flash-exp",
+    // model: "models/gemini-2.0-flash-exp",
+    model: "models/gemini-2.0-flash-live-001",
   });
   const [volume, setVolume] = useState(0);
 
